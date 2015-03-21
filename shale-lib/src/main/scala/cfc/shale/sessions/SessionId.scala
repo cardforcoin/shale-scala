3ba390432e4b4396df80354be4dfe5e5bc8c8ab1
Chris Martin <ch.martin@gmail.com>
2015-03-21 08:19:23 -0700

    add comment on shale SessionId

--- shale-lib/src/main/scala/cfc/shale/sessions/SessionId.scala
+++ shale-lib/src/main/scala/cfc/shale/sessions/SessionId.scala
@@ -3,6 +3,8 @@ package cfc.shale.sessions
 import scalaz.BijectionT._
 import scalaz.Scalaz._
 
+/** An arbitrary value identifying a session. Note that this id is assigned by Shale,
+  * and it is different from the session id assigned to a Selenium webdriver. */
 case class SessionId(value: String)
 
 object SessionId {
