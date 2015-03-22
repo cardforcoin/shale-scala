f78f248d9dc92d7c4a6c57198abc5f9f3d4ea7da
Chris Martin <ch.martin@gmail.com>
2015-03-21 19:13:55 -0700

    whitespace

--- redis-client/src/main/scala/cfc/shale/redis_client/containers/RedisContainer.scala
+++ redis-client/src/main/scala/cfc/shale/redis_client/containers/RedisContainer.scala
@@ -10,14 +10,12 @@ import scalaz._
 trait RedisContainer[A] { self =>
 
   def get: RedisCommand[A]
-  
+
   def set(value: A): RedisCommand[Unit]
 
   def biject[B](implicit bijection: Bijection[A, B]): RedisContainer[B] =
     new RedisContainer[B] {
-
       override def get = self.get.map(bijection.to)
-
       override def set(value: B) = self.set(bijection.from(value))
     }
 }
