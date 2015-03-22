00b6f26298e4f8293d829a00546fc7626864cb2e
Chris Martin <ch.martin@gmail.com>
2015-03-21 19:41:26 -0700

    add NodeAddress string bijection

--- shale-core/src/main/scala/cfc/shale/core/NodeAddress.scala
+++ shale-core/src/main/scala/cfc/shale/core/NodeAddress.scala
@@ -1,3 +1,12 @@
 package cfc.shale.core
 
+import scalaz.BijectionT._
+import scalaz.Scalaz._
+
 case class NodeAddress(uri: String)
+
+object NodeAddress {
+
+  implicit val stringBijection: Bijection[String, NodeAddress] =
+    bijection[Id, Id, String, NodeAddress](NodeAddress(_), _.uri)
+}
