a54a0a13bf7ef2ca72d565feb44e39a8611b606e
Chris Martin <ch.martin@gmail.com>
2015-03-21 18:49:52 -0700

    move shale redis commands into shale-redis project

--- shale-redis/src/main/scala/cfc/shale/redis/package.scala
+++ shale-redis/src/main/scala/cfc/shale/redis/package.scala
@@ -1,16 +1,18 @@
 package cfc.shale
-package nodes
 
-import cfc.shale.core.NodeId
-import cfc.shale.redis.Node
-
-import scalaz._, Scalaz._
+import cfc.shale.core.{NodeId, ShaleSessionId}
 import cfc.shale.redis_client.commands._
+import cfc.shale.redis_client.containers.RedisContainer.BijectionLifter
 import cfc.shale.redis_client.containers._
 
 import scala.collection.immutable.Set
+import scalaz.Scalaz._
+import scalaz._
 
 package object redis {
+
+  // Hack around a bug in IntelliJ
+  import cfc.shale.redis.Node
 
   val nodeIds = RedisGetStringSet("node-ids").map(_.map(NodeId(_)))
 
@@ -73,4 +75,48 @@ package object redis {
         )
       )
     }
+  val getSessionIds = RedisGetStringSet("session-ids").map(_.map(ShaleSessionId(_)))
+
+  def nodeId(sessionId: ShaleSessionId): RedisContainer[Option[NodeId]] =
+    RedisStringOption(s"session/${sessionId.value}/node-id")
+      .biject[Option[NodeId]](NodeId.stringBijection.liftInto[Option])
+
+  def getNodeUrl(sessionId: ShaleSessionId): RedisCommand[Option[String]] =
+    for {
+      nodeIdOption <- nodeId(sessionId).get
+      nodeUrlOption <- nodeIdOption match {
+        case None =>
+          RedisCommand.now(None)
+        case Some(nodeId: NodeId) =>
+          nodeUrl(nodeId).get
+      }
+    }
+    yield nodeUrlOption
+
+  def getSessionTags(sessionId: ShaleSessionId) =
+    RedisGetStringSet(s"session/${sessionId.value}/tags")
+
+  def reserved(sessionId: ShaleSessionId) =
+    RedisStringOption(s"session/${sessionId.value}/reserved")
+
+  def currentUrl(sessionId: ShaleSessionId) =
+    RedisStringOption(s"session/${sessionId.value}/current-url")
+
+  def browserName(sessionId: ShaleSessionId) =
+    RedisStringOption(s"session/${sessionId.value}/browser-name")
+
+  def webDriverId(sessionId: ShaleSessionId) =
+    RedisStringOption(s"session/${sessionId.value}/webdriver-id")
+
+  def getSession(sessionId: ShaleSessionId):
+      RedisCommand[Session] = {
+    RedisCommand.reduceUnordered(Stream(
+      nodeId(sessionId).get.map(x => Session(nodeId=x)),
+      getSessionTags(sessionId).map(x => Session(tags=x)),
+      reserved(sessionId).get.map(x => Session(reserved=x)),
+      currentUrl(sessionId).get.map(x => Session(currentUrl=x)),
+      browserName(sessionId).get.map(x => Session(browserName=x)),
+      webDriverId(sessionId).get.map(x => Session(webDriverId=x))
+    ))(Reducer.identityReducer)
+  }
 }
